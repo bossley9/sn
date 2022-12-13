6c9a3ad9ce1c9665601c236046d2688935393eda
sam <bossley.samuel@gmail.com>
2022-12-12 19:06:49 -0800

    move argument handling to separate Run function

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,10 @@
-// vim: fdm=indent
 package main
 
 import (
 	"os"
 
-	l "git.sr.ht/~bossley9/sn/pkg/logger"
 	sn "git.sr.ht/~bossley9/sn/pkg/sn"
 )
-
-func printUsage() {
-	args := []string{
-		"[no arg]  download and sync with server, then open the project directory with $EDITOR",
-		"c         clear auth, reset cache and remove all notes",
-		"d         download and sync with server",
-		"h         view this help usage",
-		"r         refetch all notes",
-		"u         upload and sync with server",
-	}
-
-	l.PrintInfo("\n")
-	l.PrintInfo("Usage: sn [arg]\n")
-	for _, arg := range args {
-		l.PrintInfo(arg + "\n")
-	}
-	l.PrintInfo("\n")
-}
 
 func main() {
 	args := os.Args
@@ -33,20 +13,5 @@ func main() {
 		arg = args[1]
 	}
 
-	switch arg {
-	case "":
-		sn.OpenProjectDir()
-	case "c":
-		sn.Clear()
-	case "d":
-		sn.DownloadSync(false)
-	case "h":
-		printUsage()
-	case "r":
-		sn.DownloadSync(true)
-	case "u":
-		sn.UploadSync()
-	default:
-		printUsage()
-	}
+	sn.Run(arg)
 }
--- pkg/sn/sn.go
+++ pkg/sn/sn.go
@@ -1,4 +1,3 @@
-// vim: fdm=indent
 package sn
 
 import (
@@ -8,181 +7,223 @@ import (
 	l "git.sr.ht/~bossley9/sn/pkg/logger"
 )
 
-func printFatalAndExit(err error) {
-	l.PrintError(err)
-	l.PrintError("\nFatal error. Exiting.\n")
-	os.Exit(1)
+func Run(arg string) {
+	l.PrintPlain("\n")
+	var err error
+
+	switch arg {
+	case "":
+		err = openProjectDir()
+	case "c":
+		err = clear()
+	case "d":
+		err = downloadSync(false)
+	case "h":
+		printUsage()
+	case "r":
+		err = downloadSync(true)
+	case "u":
+		err = uploadSync()
+	default:
+		printUsage()
+	}
+
+	if err != nil {
+		l.PrintError(err)
+		l.PrintError("\nFatal error. Exiting.\n")
+		os.Exit(1)
+	}
+
+	l.PrintPlain("\n") // ensure terminal fg has reverted back to default color
 }
 
-func OpenProjectDir() {
+func printUsage() {
+	args := []string{
+		"[no arg]  download and sync with server, then open the project directory with $EDITOR",
+		"c         clear auth, reset cache and remove all notes",
+		"d         download and sync with server",
+		"h         view this help usage",
+		"r         refetch all notes",
+		"u         upload and sync with server",
+	}
+
+	l.PrintInfo("Usage: sn [arg]\n")
+	for _, arg := range args {
+		l.PrintInfo(arg + "\n")
+	}
+}
+
+func openProjectDir() error {
 	l.PrintInfo("Initializing client... ")
 	client, err := c.NewClient()
 	if err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Authenticating with server... ")
 	if err := client.Authenticate(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Connecting to socket... ")
 	if err := client.Connect(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Accessing notes... ")
 	if err := client.OpenBucket("note"); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Syncing client... ")
 	if err := client.Sync(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	client.Disconnect() // disconnect after sync to prevent timeout
 	l.PrintInfo("done.\n")
 
 	// open project
 	if err := client.OpenProjectDir(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 
 	l.PrintInfo("Searching for local diffs... ")
 	diffs := client.GetLocalDiffs()
 	if len(diffs) == 0 {
-		l.PrintWarning("no local diffs found.")
-		l.PrintPlain("\n")
-		os.Exit(0)
+		l.PrintWarning("no local diffs found.\n")
+		return nil
 	}
 	l.PrintInfo("done.\n")
 
 	// reconnect after edits
 	l.PrintInfo("Authenticating with server... ")
 	if err := client.Authenticate(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Connecting to socket... ")
 	if err := client.Connect(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	defer client.Disconnect()
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Accessing notes... ")
 	if err := client.OpenBucket("note"); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Uploading diffs... ")
 	if err := client.Upload(diffs); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
+	return nil
 }
 
-func Clear() {
+func clear() error {
 	l.PrintInfo("Initializing client... ")
 	client, err := c.NewClient()
 	if err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Clearing data... ")
 	if err := client.Clear(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
+	return nil
 }
 
-func DownloadSync(reset bool) {
+func downloadSync(reset bool) error {
 	l.PrintInfo("Initializing client... ")
 	client, err := c.NewClient()
 	if err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Authenticating with server... ")
 	if err := client.Authenticate(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Connecting to socket... ")
 	if err := client.Connect(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	defer client.Disconnect()
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Accessing notes... ")
 	if err := client.OpenBucket("note"); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	if reset {
 		l.PrintInfo("Refetching... ")
 		if err := client.RefetchSync(); err != nil {
-			printFatalAndExit(err)
+			return err
 		}
 	} else {
 		l.PrintInfo("Syncing client... ")
 		if err := client.Sync(); err != nil {
-			printFatalAndExit(err)
+			return err
 		}
 	}
 	l.PrintInfo("done.\n")
+	return nil
 }
 
-func UploadSync() {
+func uploadSync() error {
 	l.PrintInfo("Initializing client... ")
 	client, err := c.NewClient()
 	if err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Searching for local diffs... ")
 	diffs := client.GetLocalDiffs()
 	if len(diffs) == 0 {
-		l.PrintWarning("no local diffs found.")
-		l.PrintPlain("\n")
-		os.Exit(0)
+		l.PrintWarning("no local diffs found.\n")
+		return nil
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Authenticating with server... ")
 	if err := client.Authenticate(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Connecting to socket... ")
 	if err := client.Connect(); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	defer client.Disconnect()
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Accessing notes... ")
 	if err := client.OpenBucket("note"); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
 
 	l.PrintInfo("Uploading diffs... ")
 	if err := client.Upload(diffs); err != nil {
-		printFatalAndExit(err)
+		return err
 	}
 	l.PrintInfo("done.\n")
+	return nil
 }
